c2c9bb27db499e495ce95d957afd3e463a731e9f
DAMMYSTANDARD <alukodammystandard2020@gmail.com>
2025-10-27 05:30:26 -0700

    redirects

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Login from "./components/login";
+import Signup from "./components/signup";
+import Dashboard from "./pages/dashboard";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function APP() {
+return (
+<>
+<Routes>
+<Route path="/" element={<Login />} />
+<Route path="/signup" element={<Signup />} />
+<Route path="/dashboard" element={<Dashboard />} />
+</Routes>
+<ToastContainer
+position="top-center"
+autoClose={3000}
+hideProgressBar={false}
+newestOnTop
+closeOnClick
+pauseOnHover
+draggable
+/>
+</>
+);
 }
-
-export default App
