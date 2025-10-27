9f89dac10064152c74af7c49b1e852060740c436
DAMMYSTANDARD <alukodammystandard2020@gmail.com>
2025-10-27 05:33:31 -0700

    login

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,23 @@ import Login from "./components/login";
 import Signup from "./components/signup";
 import Dashboard from "./pages/dashboard";
 
-
 export default function APP() {
-return (
-<>
-<Routes>
-<Route path="/" element={<Login />} />
-<Route path="/signup" element={<Signup />} />
-<Route path="/dashboard" element={<Dashboard />} />
-</Routes>
-<ToastContainer
-position="top-center"
-autoClose={3000}
-hideProgressBar={false}
-newestOnTop
-closeOnClick
-pauseOnHover
-draggable
-/>
-</>
-);
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        draggable
+      />
+    </>
+  );
 }
